5d1055647bab5a534c0842eec36a38897e6bf1df
2romanroyce8 <2romanroyce8@gmail.com>
2026-09-05 05:58:50 -0700

    Move AI Presence Score/Why This Matters section to position 4 on the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -19,8 +19,8 @@ export default function HomePage() {
         <Hero />
         <PlatformsSection />
         <ShiftSection />
-        <AnalyzeSection />
         <AlgorithmSection />
+        <AnalyzeSection />
         <ProcessSection />
       </BusinessLookupProvider>
       <PricingSection />
